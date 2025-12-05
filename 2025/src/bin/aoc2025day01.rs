97358b37d83b0e4601a7906eac57d7c3f5e6465b
Aaron Loucks <aloucks@cofront.net>
2025-12-05 15:18:21 -0500

    2025 day 01

---
+++ 2025/src/bin/aoc2025day01.rs
@@ -0,0 +1,29 @@
+struct Dial {
+    position: i32,
+}
+
+impl Dial {
+    fn new() -> Self {
+        Self { position: 50 }
+    }
+
+    fn turn(&mut self, amount: i32) -> i32 {
+        let new_position = (self.position + amount).rem_euclid(100);
+        self.position = new_position;
+        return new_position;
+    }
+}
+
+fn main() {
+    let input = include_str!("aoc2025day01_input.txt");
+    let mut dial = Dial::new();
+    let mut password = 0;
+    for line in input.lines() {
+        let val: i32 = line[1..].parse().unwrap();
+        let direction = if "L" == &line[0..1] { -1 } else { 1 };
+        if dial.turn(val * direction) == 0 {
+            password += 1;
+        }
+    }
+    println!("{:?}", password);
+}
